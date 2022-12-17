935a4bf0ab518334cd2961c61f9a17bb8408d1e2
Ashish Salaskar <ashishsalaskar1@gmail.com>
2022-12-17 20:22:14 +0530

    GoLang Basics added

--- Code_Snippets/Go Lang/Beginner Learning/01hello/main.go
+++ Code_Snippets/Go Lang/Beginner Learning/01hello/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Print("HELLO FROM GOLANG")
+	var username string = "ahsish"
+	fmt.Printf("The username is %T \n", username)
+	fmt.Println("HELLO FROM GOLANG")
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter some number")
+	input, _ := reader.ReadString('\n') // read till \n in encountered
+
+	numInput, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Added 1 to number: ", numInput+1)
+	}
+
+	fmt.Println("The value that you have read is: ", numInput)
 }
---
+++ Code_Snippets/Go Lang/Beginner Learning/02time/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	presentTime := time.Now()
+	fmt.Println(presentTime)
+	fmt.Println(presentTime.Format("01-02-2006 15:04:05 Monday"))
+
+	createdDate := time.Date(2020, time.June, 10, 12, 12, 0, 0, time.UTC)
+	fmt.Println("My created date: ", createdDate)
+}
