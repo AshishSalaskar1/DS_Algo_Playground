c2792a63f1640ebedb13e363fe356f1d293e7853
Ashish Salaskar <ashishsalaskar1@gmail.com>
2023-04-16 19:45:09 +0530

    Py to Jupyter Notebook

---
+++ Track/Striver_DSA/Arrays/.ipynb_checkpoints/Longest_Subarray_Sum-checkpoint.py
@@ -0,0 +1,23 @@
+
+"""
+Longest Subarray with given sum (K) -> ONLY POSITIVES
+== Link
+https://www.codingninjas.com/codestudio/problems/ninja-and-sorted-arrays_1214628
+
+== Explain
+PREFIX SUM TECHNIQUE
+
+"""
+
+def longest_subarray_sum(arr, n):
+    """
+    Return length of the maximum subarray with given sum=k
+    """
+
+
+
+
+
+arr, k  = [7,1,6,0], 7 # Length of the longest subarray with sum K is 3
+arr, k  = [2,3,5,1,9], 10 # Length of the longest subarray with sum K is 3
+ 
