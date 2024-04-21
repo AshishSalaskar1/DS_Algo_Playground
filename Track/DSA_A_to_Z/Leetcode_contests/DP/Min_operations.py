d84be187070c5d6ea7cf706459d5154f6ab24e19
Ashish Salaskar <30905250+AshishSalaskar1@users.noreply.github.com>
2024-04-21 20:10:05 +0530

    Leetcode Weekly Contest problems

--- Track/DSA_A_to_Z/Leetcode_contests/DP/Min_operations.py
+++ Track/DSA_A_to_Z/Leetcode_contests/DP/Min_operations.py
@@ -1,8 +1,18 @@
 """
 Link: https://leetcode.com/problems/minimum-number-of-operations-to-satisfy-conditions/description/
+Greedy wont work -> DP NEEDS TO BE DONE 
+- Because in greedy you pick one and then move one (But here once you pick up at that time it may be best, 
+    but later than can be a worse choice later looking at the further choices which you dont have a future insight about)
+- Here first col, best is 
+- First col: best is replace 1<->2 -> but then in next row you cant pick 2 (THIS IS WORSE)
+ 1. Pick to replace 1<->2 => takes 1 replacement
+ 2. Here everything is 2, but you cant pick 2 which you already picked in last col (THIS WAS A BAD DECISION IN THE PAST)
+ - IDEAL CASE: You should have replaced all 1s in col1 and then in second col you shouldnt have to do any replacements
+[2 2 3]
+[1 2 5]
+[1 2 5]
+[1 2 4]
 
-Greedy wont work -> DP NEEDS TO BE DONE
- 
 INTUITIONS
 - You can only fill numbers 0-9
 - Each ele must be equal to ele below it also -> means whole col has same number
@@ -12,6 +22,7 @@ INTUITIONS
 SOLUTION
 - DP inputs -> prev_filled_val, cur_col
 - Try filling 0-9 except prev_filled in cur_col and then pick it + pass it to next row
+ 
 """
 
 class Solution:
@@ -27,10 +38,10 @@ class Solution:
 
         min_repl = float("inf")
         for x in range(10):
-            if x!=prev:
+            if x!=prev: # dont pick previous filled value in col
                 min_repl = min(
                     min_repl,
-                    (nr-freq[col][x]) + self.solve(col+1, x, freq, nr, nc) 
+                    (nr-freq[col][x]) + self.solve(col+1, x, freq, nr, nc)  # you pick element "x"
                 )
         
         self.dp[idx] = min_repl
@@ -48,9 +59,3 @@ class Solution:
 
         res = self.solve(0, -1, freq, nr, nc)
         return res
-
-
-
-
-
-        
