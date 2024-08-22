0aa8c8157de155576efbc9c306c4aca79aaeb270
Ashish Salaskar <ashishsalaskar1@gmail.com>
2024-08-22 15:25:52 +0530

    Update heap questions

--- Track/DSA_A_to_Z/Heaps/Median_in_Stream.py
+++ Track/DSA_A_to_Z/Heaps/Median_in_Stream.py
@@ -1,3 +1,34 @@
+"""
+PROBLEM: https://leetcode.com/problems/find-median-from-data-stream/description/
+
+SOLUTION:
+
+=> INTUITION
+- Consider array of numbers. There are 2 options
+1. EVEN Numbers
+    - Pick 2 mids and mean of those 2
+2. ODD Numbers: 
+    - Pick mid element
+
+- Assume the array is sorted and divided into 2 parts (each array is size of n//2-EVEN or n//2+1-ODD)
+1. EVEN: L=[1,2,3,4] R=[5,6,7,8] 
+    - Here median = 4+5/2 = max(LEFT) + max(RIGHT) // 2 
+
+2a. ODD: L=[1,2,3] R=[4,5,6,7]
+    - min(RIGHT)
+2b. ODD: L=[1,2,3,4] R=[5,6,7]
+    - max(LEFT)
+
+EVEN = max(LEFT) + max(RIGHT) // 2 
+ODD = in case len(LEFT) > len(RIGHT) => max(LEFT)
+ODD = in case len(LEFT) < len(RIGHT) => MIN(RIGHT)
+
+IMPLEMENTATION NEEDED
+- 2 Sorted arrays LEFT and RIGHT which may differ only by 1 in terms of size
+- LEFT array is a maxHeap -> you need max of left
+- RIGHT array is a minHeap -> you need min of right
+"""
+
 from queue import PriorityQueue
 class MedianFinder:
 
