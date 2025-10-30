cbb7034887fb5e7a10ed4bb9248c2888d99dd226
AshishSalaskar1 <ashishsalaskar1@gmail.com>
2025-10-31 00:36:27 +0530

    Update

--- Track/DSA_A_to_Z/Graphs/Disjoint_Set_OR_Union_Find/Merge_Accounts.py
+++ Track/DSA_A_to_Z/Graphs/Disjoint_Set_OR_Union_Find/Merge_Accounts.py
@@ -29,6 +29,20 @@ res = []
     
 
 -> Convert your parent_id into names and then sort emails based
+
+
+💡Tricky Part: Since account names can be duplicates -> how do you differentiate 2 johns?
+APPROACH 1
+- Assign index to each acc_name and then you just keep on union these acc_indexes
+- Maintain dict to check which email belongs to which acc_idx, then if its already seen UNION( current, seen_acc_idx)
+
+APPROACH 2
+- Consider unioning <email1, email2>. But store dict<email: acc_name>
+- For each union first email with remanining ones. Iterate all emails -> find their parent and add to list 
+- Then at last, replace parent email with its acc_name
+
+https://leetcode.com/problems/accounts-merge/submissions/1816252745/"
+
 """ 
 from typing import List
 
@@ -90,5 +104,70 @@ class Solution:
         
         return [[accounts[acc_id][0],*sorted(list(emails))] for acc_id, emails in res.items()]
 
+
+
+from collections import defaultdict
+class UF:
+    def __init__(self):
+        self.parent = {}
+        self.size = {}
+    
+    def find_parent(self, node):
+        if self.parent[node] == node:
+            return node
+        
+        self.parent[node] = self.find_parent(self.parent[node])
+        return self.parent[node]
+    
+    def union(self, email1, email2):
+        for email in [email1, email2]:
+            if email not in self.parent: 
+                self.parent[email] = email
+                self.size[email] = 1
+        
+        p1, p2 = self.find_parent(email1), self.find_parent(email2)
+
+        if p1 == p2: return
+        if self.size[p1] < self.size[p2]:
+            self.parent[p1] = p2
+            self.size[p2] += self.size[p1]
+        else:
+            self.parent[p2] = p1
+            self.size[p1] += self.size[p2]
+        
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        email_map = {}
+        uf = UF()
+        for account in accounts:
+            
+            acc_name = account[0]
+            first_email = account[1]
+
+            for email in account[1:]:
+                email_map[email] = acc_name
+                uf.union(first_email, email)
+        
+        res = defaultdict(list)
+        for email, acc_name in email_map.items():
+            par = uf.find_parent(email)
+            res[par].append(email)
+        
+        final_res = []
+
+        for parent_email, emails in res.items():
+            final_res.append(
+                [
+                    email_map[parent_email],
+                    *(sorted(emails))
+                ]
+            )
+        
+        return final_res
+
+
+
+            
 
         
